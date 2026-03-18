22987c8bd109c4cd7058b5aa9a1f63c789aa88ea
César Gabriel Zambrano Figueroa <zamcega1804@hotmail.com>
2026-03-18 12:36:02 -0500

    feat: cambio de mensaje

--- Ejercicio1/src/mundo/Main.java
+++ Ejercicio1/src/mundo/Main.java
@@ -3,7 +3,7 @@ package mundo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, estoy iniciando en mi entrenamiento profesional");
+		System.out.println("Hola, estoy haciendo mi segundo ejercicio sobre cambio en este mensaje");
 	}
 
 }
