2b5816167e345cd9aef8adcbec93d33e8b350ca6
César Gabriel Zambrano Figueroa <zamcega1804@hotmail.com>
2026-03-18 12:37:27 -0500

    feat: mensaje personalizado

--- Ejercicio1/src/mundo/Main.java
+++ Ejercicio1/src/mundo/Main.java
@@ -3,7 +3,7 @@ package mundo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, estoy haciendo mi segundo ejercicio sobre cambio en este mensaje");
+		System.out.println("Hola César, este es tu progreso");
 	}
 
 }
