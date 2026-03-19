55f15974a8619da866023bf9c7f9a8b70bac99c5
César Gabriel Zambrano Figueroa <zamcega1804@hotmail.com>
2026-03-18 22:29:28 -0500

    feat: uso de condicionales

--- Ejercicio1/src/mundo/Main.java
+++ Ejercicio1/src/mundo/Main.java
@@ -7,9 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Ingrese su nombre: ");
-		String nombre = sc.nextLine();
+		System.out.print("Ingrese la edad: ");
+		int edad = sc.nextInt();
 		
-		System.out.println("Hola " + nombre);
+		if (edad >= 18) {
+			System.out.println("Mayor de edad");
+		} else {
+			System.out.println("Menor de edad");
+		}
 	}
 }
