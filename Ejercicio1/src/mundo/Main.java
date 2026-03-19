ef1e5b79c2406bbe7b3cf5ec194add493cbb830b
César Gabriel Zambrano Figueroa <zamcega1804@hotmail.com>
2026-03-18 22:31:09 -0500

    feat: uso de for

--- Ejercicio1/src/mundo/Main.java
+++ Ejercicio1/src/mundo/Main.java
@@ -5,15 +5,8 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Ingrese la edad: ");
-		int edad = sc.nextInt();
-		
-		if (edad >= 18) {
-			System.out.println("Mayor de edad");
-		} else {
-			System.out.println("Menor de edad");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Número: " + i);
 		}
 	}
 }
