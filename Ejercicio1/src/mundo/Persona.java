ab3eab528eaa89b5f3777d3285688251b722f5d9
César Gabriel Zambrano Figueroa <zamcega1804@hotmail.com>
2026-03-18 22:48:36 -0500

    feat: getters and setters

--- Ejercicio1/src/mundo/Persona.java
+++ Ejercicio1/src/mundo/Persona.java
@@ -12,5 +12,17 @@ public class Persona {
 	public String getNombre() {
 		return nombre;
 	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
 
